f0a4675596b24c1a975b46c721ee5c336f15ffbb
Anil <anil.mer4@gmail.com>
2018-08-26 14:54:08 +0530

    title changed

--- caretaker/www/js/config.js
+++ caretaker/www/js/config.js
@@ -2,7 +2,7 @@ if (window.location.hostname == "localhost") {
     angular.module('sspf.config', [])
             .constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
 //.constant('API_URL', 'http://sspf.in/sspfapp/sspfapi/v1')
-            .constant('API_URL', 'http://localhost/sspf/api/v1')
+            .constant('API_URL', 'http://localhost/api/v1')
             .constant('GCM_SENDER_ID', '523426080086')
 
             ;
